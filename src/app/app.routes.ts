0a89144e4e3a052c2e7a0ce3286808a8b94d9b0f
Luis <lmejias@telynet.com>
2021-11-08 12:51:55 -0300

    add some changes according to sugestions

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,12 @@
 import { Routes } from '@angular/router';
-// import { AuthGuard } from './auth/auth.guard';
 export const appRoutes: Routes = [
   {
     path: 'app',
-    // canActivate: [AuthGuard],
     loadChildren: () =>
       import(`./app.module`).then((module) => module.AppModule),
   },
   {
-    path: 'login',
-    loadChildren: () =>
-      import(`./modules/login/login.module`).then(
-        (module) => module.LoginModule
-      ),
-  },
-  {
     path: 'dashboard',
-    // canActivate: [AuthGuard],
     loadChildren: () =>
       import(`./modules/dashboard/dashboard.module`).then(
         (module) => module.DashboardModule
@@ -24,7 +14,6 @@ export const appRoutes: Routes = [
   },
   {
     path: 'heroes',
-    // canActivate: [AuthGuard],
     loadChildren: () =>
       import(`./modules/heroes/heroes.module`).then(
         (module) => module.HeroesModule
--- src/app/modules/heroes/components/heroes-table/heroes-table.component.ts
+++ src/app/modules/heroes/components/heroes-table/heroes-table.component.ts
@@ -1,8 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
-import { FormGroup } from '@angular/forms';
-import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
-import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
 import { MatTableDataSource } from '@angular/material/table';
 import { Subscription } from 'rxjs';
 import { Heroe } from '../../models/heroe.model';
@@ -15,15 +12,12 @@ import { Heroe } from '../../models/heroe.model';
 export class HeroesTableComponent implements OnInit {
   @Input() dataSource = new MatTableDataSource<Heroe>([]);
   @Output() handleDataEdit = new EventEmitter();
-  @Output() handleDataDelete = new EventEmitter(); 
-
+  @Output() handleDataDelete = new EventEmitter();
   displayedColumns: string[] = ['id', 'name', 'biography', 'image', 'appearance', 'house', 'edit', 'delete'];
   isLoadingResults: boolean = true;
   isRateLimitReached: boolean = false;
   resultsLength: number = 0;
   originalDataSource = new MatTableDataSource<Heroe>([]);
-  horizontalPosition: MatSnackBarHorizontalPosition = 'center';
-  verticalPosition: MatSnackBarVerticalPosition = 'top';
   subscription = new Subscription();
   @ViewChild(MatPaginator) paginator: MatPaginator;
   constructor() {}
